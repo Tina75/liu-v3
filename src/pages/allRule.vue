31f761b07736855258a273c87fd22391fc2f3b0c
youer name <you@example.com>
2018-06-09 14:06:07 +0800

    daily commit

--- src/pages/allRule.vue
+++ src/pages/allRule.vue
@@ -33,32 +33,32 @@ export default {
     }
   },
   methods: {
-    handleTabsEdit(targetName, action) {
+    handleTabsEdit( targetName, action) {
       if (action === 'add') {
-        let newTabName = ++this.tabIndex + '';
+        let newTabName = ++this.tabIndex + ''
         this.editableTabs.push({
           title: 'New Tab',
           name: newTabName,
           content: 'New Tab content'
-        });
-        this.editableTabsValue = newTabName;
+        })
+        this.editableTabsValue = newTabName
       }
       if (action === 'remove') {
-        let tabs = this.editableTabs;
-        let activeName = this.editableTabsValue;
+        let tabs = this.editableTabs
+        let activeName = this.editableTabsValue
         if (activeName === targetName) {
           tabs.forEach((tab, index) => {
             if (tab.name === targetName) {
-              let nextTab = tabs[index + 1] || tabs[index - 1];
+              let nextTab = tabs[index + 1] || tabs[index - 1]
               if (nextTab) {
-                activeName = nextTab.name;
+                activeName = nextTab.name
               }
             }
-          });
+          })
         }
 
-        this.editableTabsValue = activeName;
-        this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+        this.editableTabsValue = activeName
+        this.editableTabs = tabs.filter(tab => tab.name !== targetName)
       }
     }
   }
